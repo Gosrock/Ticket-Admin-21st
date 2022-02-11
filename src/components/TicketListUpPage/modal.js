1afcd898a561ad64ebdc56da40ec98cd577d0f32
unknown <nohtak1213@gmail.com>
2022-02-12 00:49:57 +0900

    어드민티켓 발급 구현 완료

---
+++ src/components/TicketListUpPage/modal.js
@@ -0,0 +1,72 @@
+import { Form, Input, Button, Space } from 'antd';
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
+import axios from 'axios';
+
+export const TicketModal = () => {
+  const [form] = Form.useForm();
+
+  const onSubmitHandler = values => {
+    const fetchData = async () => {
+      try {
+        const accountNameList = values.accountNameList;
+        console.log('newList:', accountNameList);
+        const response = await axios.post('/admin/tickets', {
+          accountNameList
+        });
+        console.log(response);
+      } catch (e) {
+        console.log('submit error:', e.response.data);
+      }
+    };
+    fetchData();
+    form.resetFields(['dynamic_form_nest_item']);
+  };
+
+  return (
+    <Form
+      name="dynamic_form_nest_item"
+      autoComplete="off"
+      onFinish={onSubmitHandler}
+    >
+      <Form.List name="accountNameList">
+        {(fields, { add, remove }) => (
+          <>
+            {fields.map(({ key, name, ...restField }) => (
+              <Space
+                key={key}
+                style={{ display: 'flex', marginBottom: 8 }}
+                align="baseline"
+              >
+                <Form.Item
+                  style={{ width: '200px' }}
+                  {...restField}
+                  name={[name, 'accountName']}
+                  rules={[{ required: true, message: '이름을 입력하세요' }]}
+                >
+                  <Input placeholder="발급할 이름을 입력하세요" />
+                </Form.Item>
+
+                <MinusCircleOutlined onClick={() => remove(name)} />
+              </Space>
+            ))}
+            <Form.Item>
+              <Button
+                type="dashed"
+                onClick={() => add()}
+                block
+                icon={<PlusOutlined />}
+              >
+                추가
+              </Button>
+            </Form.Item>
+          </>
+        )}
+      </Form.List>
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
